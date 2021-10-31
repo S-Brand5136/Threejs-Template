4d1dd55fa13609b0459aa5a9f7413fe7124f4b2f
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-10-31 16:39:36 -0600

    Add base lighting class code

--- build/Lighting.js
+++ build/Lighting.js
@@ -1,8 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Lighting = void 0;
+var three_1 = require("three");
 var Lighting = /** @class */ (function () {
-    function Lighting() {
+    function Lighting(scene) {
+        var geometry = new three_1.BoxBufferGeometry();
+        var roomMaterial = new three_1.MeshStandardMaterial({
+            metalness: 0,
+            side: three_1.FrontSide,
+        });
+        var room = new three_1.Mesh(geometry, roomMaterial);
+        room.scale.set(31, 28, 28);
+        var mainLight = new three_1.PointLight(0xffffff, 35, 256, 1);
+        mainLight.position.set(0.418, 16.199, 0.3);
+        var hemiLight = new three_1.HemisphereLight(0xffffff, 0xffffff, 0.5);
+        var shadowLight = new three_1.DirectionalLight(0xffffff, 1);
+        shadowLight.position.set(1.2, 1, 0.2);
+        shadowLight.castShadow = true;
+        shadowLight.shadow.mapSize.set(2048, 2048);
+        var lightOne = new three_1.DirectionalLight(0xffffff, 1);
+        lightOne.position.set(5, 3, 1);
+        var lightTwo = new three_1.DirectionalLight(0xffffff, 1);
+        lightTwo.position.set(5, 3, 1);
+        var lightThree = new three_1.DirectionalLight(0xffffff, 1);
+        lightThree.position.set(5, 3, 1);
+        var lightFour = new three_1.DirectionalLight(0xffffff, 1);
+        lightFour.position.set(5, 3, 1);
+        scene.add(room, mainLight, hemiLight, shadowLight, lightOne, lightTwo, lightThree, lightFour);
     }
     return Lighting;
 }());
--- src/Lighting.ts
+++ src/Lighting.ts
@@ -1,1 +1,57 @@
-export class Lighting {}
+import {
+  BoxBufferGeometry,
+  DirectionalLight,
+  FrontSide,
+  Group,
+  HemisphereLight,
+  Mesh,
+  MeshStandardMaterial,
+  PointLight,
+} from "three";
+
+export class Lighting {
+  constructor(scene: Group) {
+    const geometry = new BoxBufferGeometry();
+
+    const roomMaterial = new MeshStandardMaterial({
+      metalness: 0,
+      side: FrontSide,
+    });
+
+    const room = new Mesh(geometry, roomMaterial);
+    room.scale.set(31, 28, 28);
+
+    const mainLight = new PointLight(0xffffff, 35, 256, 1);
+    mainLight.position.set(0.418, 16.199, 0.3);
+
+    const hemiLight = new HemisphereLight(0xffffff, 0xffffff, 0.5);
+
+    const shadowLight = new DirectionalLight(0xffffff, 1);
+    shadowLight.position.set(1.2, 1, 0.2);
+    shadowLight.castShadow = true;
+    shadowLight.shadow.mapSize.set(2048, 2048);
+
+    const lightOne = new DirectionalLight(0xffffff, 1);
+    lightOne.position.set(5, 3, 1);
+
+    const lightTwo = new DirectionalLight(0xffffff, 1);
+    lightTwo.position.set(5, 3, 1);
+
+    const lightThree = new DirectionalLight(0xffffff, 1);
+    lightThree.position.set(5, 3, 1);
+
+    const lightFour = new DirectionalLight(0xffffff, 1);
+    lightFour.position.set(5, 3, 1);
+
+    scene.add(
+      room,
+      mainLight,
+      hemiLight,
+      shadowLight,
+      lightOne,
+      lightTwo,
+      lightThree,
+      lightFour
+    );
+  }
+}
