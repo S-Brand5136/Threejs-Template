6e3332a8a71c42395a0223a0381aba6929a9b430
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-10-31 16:39:48 -0600

    Add base renderer class code

--- build/Renderer.js
+++ build/Renderer.js
@@ -1,8 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Renderer = void 0;
+var three_1 = require("three");
 var Renderer = /** @class */ (function () {
-    function Renderer() {
+    function Renderer(canvas) {
+        this.webGl = new three_1.WebGLRenderer({
+            canvas: canvas,
+            alpha: true,
+            antialias: true,
+            powerPreference: "high-performance",
+            preserveDrawingBuffer: true,
+        });
+        this.webGl.toneMapping = three_1.ACESFilmicToneMapping;
+        this.webGl.toneMappingExposure = 2;
+        this.webGl.autoClear = true;
+        var mPmremGenerator = new three_1.PMREMGenerator(this.webGl);
+        mPmremGenerator.compileEquirectangularShader();
+        this.webGl.outputEncoding = three_1.GammaEncoding;
+        this.webGl.physicallyCorrectLights = true;
+        this.webGl.setPixelRatio(window.devicePixelRatio);
+        this.webGl.shadowMap.enabled = true;
+        this.webGl.shadowMap.type = three_1.PCFSoftShadowMap;
     }
     return Renderer;
 }());
--- src/Renderer.ts
+++ src/Renderer.ts
@@ -1,1 +1,36 @@
-export class Renderer {}
+import {
+  ACESFilmicToneMapping,
+  GammaEncoding,
+  PCFSoftShadowMap,
+  PMREMGenerator,
+  WebGLRenderer,
+} from "three";
+
+export class Renderer {
+  public webGl: WebGLRenderer;
+
+  constructor(canvas: HTMLElement) {
+    this.webGl = new WebGLRenderer({
+      canvas,
+      alpha: true,
+      antialias: true,
+      powerPreference: "high-performance",
+      preserveDrawingBuffer: true,
+    });
+
+    this.webGl.toneMapping = ACESFilmicToneMapping;
+    this.webGl.toneMappingExposure = 2;
+    this.webGl.autoClear = true;
+
+    const mPmremGenerator = new PMREMGenerator(this.webGl);
+    mPmremGenerator.compileEquirectangularShader();
+
+    this.webGl.outputEncoding = GammaEncoding;
+    this.webGl.physicallyCorrectLights = true;
+    this.webGl.setPixelRatio(window.devicePixelRatio);
+
+    this.webGl.shadowMap.enabled = true;
+
+    this.webGl.shadowMap.type = PCFSoftShadowMap;
+  }
+}
