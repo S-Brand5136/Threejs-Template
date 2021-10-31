dba0f9b3ea15f4f306d8731b85a9fcba7443673f
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-10-31 16:38:50 -0600

    Remove unneeded controls class

--- build/Controls.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Controls = void 0;
-var Controls = /** @class */ (function () {
-    function Controls() {
-    }
-    return Controls;
-}());
-exports.Controls = Controls;
